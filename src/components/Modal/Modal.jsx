33fbac717ab424be94e433b77dcbbd465da71210
S.Lazarenko <ajaxamsterdam878@gmail.com>
2020-04-05 11:13:23 +0300

    commit2

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 import PropTypes from "prop-types";
 import styles from "./Modal.module.css";
-export default class Modal extends Component {
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.modalRef = createRef();
+  }
   static propTypes = {
     onCloseModal: PropTypes.func.isRequired,
     imageId: PropTypes.number.isRequired,
-    articles: PropTypes.array.isRequired
+    articles: PropTypes.array.isRequired,
   };
   componentDidMount() {
     window.addEventListener("keydown", this.handleESC);
@@ -13,21 +17,29 @@ export default class Modal extends Component {
   componentWillUnmount() {
     window.removeEventListener("keydown", this.handleESC);
   }
-  handleESC = e => {
+  handleESC = (e) => {
     if (e.keyCode === 27) {
       this.props.onCloseModal();
     }
   };
+  handleBgClick = (e) => {
+    const overlay = this.modalRef.current;
+    if (e.target === overlay) this.props.onCloseModal();
+  };
   render() {
-    const { imageId, articles, onCloseModal } = this.props;
+    const { imageId, articles } = this.props;
 
-    const largeImage = articles.find(article => {
+    const largeImage = articles.find((article) => {
       if (article.id === imageId) {
         return article.largeImageURL;
       }
     });
     return (
-      <div className={styles.Overlay} onClick={onCloseModal}>
+      <div
+        className={styles.Overlay}
+        onClick={this.handleBgClick}
+        ref={this.modalRef}
+      >
         <div className={styles.Modal}>
           <img src={largeImage.largeImageURL} alt={largeImage.tags} />
         </div>
@@ -35,3 +47,4 @@ export default class Modal extends Component {
     );
   }
 }
+export default Modal;
